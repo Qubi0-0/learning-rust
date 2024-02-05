916d55b7d27a8607c036cef3a6d929337d1103c4
Qubi <kuba1102@gmail.com>
2024-02-05 12:47:54 +0100

    Add parallel processing for letter frequency calculation

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,11 +1,51 @@
 use std::collections::HashMap;
+use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    todo!(
-        "Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{worker_count} workers"),
+    
+    let mut handles = vec![];
+
+    if input.is_empty() || worker_count == 0 {
+        return HashMap::new();
+    }
+
+    let text = input.join("");
+    let chunk_size = text.len() / worker_count;
+    if chunk_size != 0 {
+        let chunks: Vec<_> = text
+            .chars()
+            .collect::<Vec<_>>()
+            .chunks(chunk_size)
+            .map(|c| c.iter().collect::<String>())
+            .collect();
+        for chunk in chunks {
+            let handle = thread::spawn(move || string_counter(&chunk));
+            handles.push(handle);
         }
-    );
+    } else {
+        return HashMap::new();
+    }
+
+    let mut frequency = HashMap::new();
+
+    for handle in handles {
+        let thread_frequency = handle.join().unwrap();
+        for (key, value) in thread_frequency {
+            *frequency.entry(key).or_insert(0) += value;
+        }
+    }
+
+    frequency
+}
+
+fn string_counter(input: &str) -> HashMap<char, usize> {
+    let mut frequency = HashMap::new();
+
+    for ch in input.chars() {
+        if ch.is_alphabetic() {
+            *frequency.entry(ch.to_ascii_lowercase()).or_insert(0) += 1;
+        }
+    }
+
+    frequency
 }
