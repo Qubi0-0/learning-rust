9fd137565cf0810923a092ada0b9a6e894e9fcbe
Jakub Zeifert <kuba1102@gmail.com>
2024-02-09 13:36:30 +0100

    Implement basic logic for determining winning hand

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -3,5 +3,17 @@
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
-    todo!("Out of {hands:?}, which hand wins?")
+    if hands.len() == 1 {
+        vec![hands[0]]
+    } else {
+        todo!()
+    }
 }
+
+
+// S - Spades
+// C - Clubs
+// H - Hearts
+// D - Diamonds
+// NO RANK in Suits!
+
