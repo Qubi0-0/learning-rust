b9a66fa13f2805b3f73037f19863759da7d4f780
Jakub Zeifert <kuba1102@gmail.com>
2024-02-09 13:04:41 +0100

    Refactor is_armstrong_number function to calculate the power of each digit

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let binding = num.to_string();
+    let num_powered: u64 = binding
+        .chars()
+        .map(|x| {
+            x.to_digit(10)
+                .unwrap()
+                .pow(binding.len().try_into().unwrap()) as u64
+        })
+        .sum();
+    let mut result = false;
+    if num_powered == num as u64 {
+        result = true;
+    }
+
+    result
 }
