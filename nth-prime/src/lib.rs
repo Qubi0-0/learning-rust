74c414d3e7499ecee5e30faa5ddb8e902bb126f5
Jakub Zeifert <kuba1102@gmail.com>
2024-02-10 22:46:19 +0100

    Implement nth prime number function

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn nth(n: u32) -> u32 {
-    todo!("What is the 0-indexed {n}th prime number?")
+    let mut primes = Vec::new();
+    let mut num = 2;
+
+    while (primes.len() as u32) <= n {
+        if primes.iter().all(|&prime| num % prime != 0) {
+            primes.push(num);
+        }
+        num += 1;
+    }
+
+    primes[n as usize] 
 }
