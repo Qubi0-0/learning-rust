83329181efe9fc84129895206bcf6e18712760f4
Jakub Zeifert <kuba1102@gmail.com>
2024-02-10 22:24:33 +0100

    Refactor leap year calculation logic

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,13 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 100 == 0 {
-        if year % 400 == 0 {
-            true
-        } else {
-            false
-        }
-    } else if year % 4 == 0 {
-        true
-    } else {
-        false
-    }
+    (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))
 }
