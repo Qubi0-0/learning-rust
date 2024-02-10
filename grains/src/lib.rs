540c50f13637edab7f2939d3bf4c89521b81fee5
Jakub Zeifert <kuba1102@gmail.com>
2024-02-10 22:00:57 +0100

    Refactor square and total functions

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+    if s > 0 && s < 65 {
+        2_u64.pow(s - 1)
+    } else {
+        panic!("Square must be between 1 and 64")
+    }
 }
 
 pub fn total() -> u64 {
-    todo!();
+    let s = 64;
+    (1..=s).map(|s| 2_u64.pow(s - 1)).sum::<u64>()
 }
