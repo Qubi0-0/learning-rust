39d73c26b02d70dd3728f5502bf4c79938a69fae
Jakub Zeifert <kuba1102@gmail.com>
2024-02-12 11:34:57 +0100

    Implement prime factor calculation algorithm

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    let mut divider: u64 = 2;
+    let mut dividers: Vec<u64> = vec![];
+    let mut value = n;
+    while value != 1 {
+        if value % divider == 0 {
+            dividers.push(divider);
+            value = value / divider;
+            divider = 2;
+        } else {
+            divider = divider + 1;
+        }
+    }
+    dividers
 }
