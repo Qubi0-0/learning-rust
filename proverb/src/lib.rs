a3176782c93f60dd19ae8152fce24b8b800e2ed7
Jakub Zeifert <kuba1102@gmail.com>
2024-02-12 11:51:23 +0100

    Implement build_proverb function to generate a proverb from a list

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
+    let mut story = String::new();
+    for (idx,word) in list.iter().enumerate() {
+        if idx < list.len() {
+        story.push_str(&format!("For want of a {} the {} was lost \n",word, list[idx+1]));
+        }
+        else {
+            story.push_str(&format!("And all for the want of a {}.", list[0]));
+        }
+    }
+    story
 }
+ 
