5f4c205c45eccbbb38670e4548395e3cebe194e1
Jakub Zeifert <kuba1102@gmail.com>
2024-02-12 12:41:49 +0100

    Refactor build_proverb function to use iterator chaining

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,16 +1,20 @@
 pub fn build_proverb(list: &[&str]) -> String {
     if list.is_empty() {
-        return String::new()
+        return String::new();
     }
     let mut story = String::new();
-    for (idx, word) in list.iter().skip(1).enumerate() {
-        if idx < list.len() - 1 {
-            story.push_str(&format!(
-                "For want of a {} the {} was lost.\n",
-                list[idx], word
-            ));
-        }
-    }
+    list.iter()
+        .skip(1)
+        .enumerate()
+        .map(|(idx, word)| {
+            if idx < list.len() - 1 {
+                story.push_str(&format!(
+                    "For want of a {} the {} was lost.\n",
+                    list[idx], word
+                ));
+            }
+        })
+        .for_each(|_| {});
     story.push_str(&format!("And all for the want of a {}.", list[0]));
     story
 }
