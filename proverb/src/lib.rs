925264d741799e0110cf0cb2780d5b217b8442a2
Jakub Zeifert <kuba1102@gmail.com>
2024-02-12 12:35:47 +0100

    Refactor build_proverb function to improve readability and fix formatting

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn build_proverb(list: &[&str]) -> String {
     let mut story = String::new();
-    for (idx,word) in list.iter().enumerate() {
-        if idx < list.len() {
-        story.push_str(&format!("For want of a {} the {} was lost \n",word, list[idx+1]));
-        }
-        else {
-            story.push_str(&format!("And all for the want of a {}.", list[0]));
+    for (idx, word) in list.iter().skip(1).enumerate() {
+        if idx < list.len() - 1 {
+            story.push_str(&format!(
+                "For want of a {} the {} was lost.\n",
+                list[idx], word
+            ));
         }
     }
+    story.push_str(&format!("And all for the want of a {}.", list[0]));
     story
 }
- 
