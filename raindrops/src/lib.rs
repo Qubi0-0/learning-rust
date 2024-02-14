45b71e4467d2bdd1268172d9362d390359f1da30
Jakub Zeifert <kuba1102@gmail.com>
2024-02-14 12:51:31 +0100

    Refactor raindrops function to simplify code - improved version

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,17 +1,17 @@
 pub fn raindrops(n: u32) -> String {
     let mut result = String::new();
-    if n % 3 == 0 || n % 5 == 0 || n % 7 == 0  {
-        if n % 3 == 0 {
-            result.push_str("Pling");
-        }
-        if n % 5 == 0 {
-            result.push_str("Plang");
-        }
-        if n % 7 == 0 {
-            result.push_str("Plong");
-        }
-    } else {
-        result.push_str(&n.to_string());
+    if n % 3 == 0 {
+        result.push_str("Pling");
     }
-    result
+    if n % 5 == 0 {
+        result.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        result.push_str("Plong");
+    }
+    if result.is_empty() {
+        n.to_string()
+    } else {
+        result
+    }
 }
