82cad232655ee4cf8138801c18f8f7dc5aaef920
Jakub Zeifert <kuba1102@gmail.com>
2024-02-15 15:08:29 +0100

    Add HandRank struct and implement rank_hand function (in progress)

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -1,57 +1,43 @@
+use std::collections::LinkedList;
 use std::{collections::HashMap, vec};
+
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
+struct HandRank {
+    rank: u16,
+    tie_breaker: str,
+}
+
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
     if hands.len() == 1 {
         vec![hands[0]]
+    } else if hands.iter().all(|hand| hand.replace(" ", "").len() == 10) {
+        todo!()
     } else {
-
-        let mut points_map: HashMap<&str, u32> = HashMap::new();
-
-        for hand in hands.iter() {
-            let points = rank_hand(hand);
-            points_map.insert(hand, points);
-        }
-
-        let (best_hand, _) = points_map.iter().max_by_key(|&(_, points)| points).unwrap();
-
-        vec![*best_hand]
+        panic!("Card are not with correct lenght!!")
     }
 }
 
-fn rank_hand(hand: &str) -> u32 {
-    let mut rank_to_value: HashMap<&str, u32> = HashMap::new();
-    rank_to_value.insert("2", 2);
-    rank_to_value.insert("3", 3);
-    rank_to_value.insert("4", 4);
-    rank_to_value.insert("5", 5);
-    rank_to_value.insert("6", 6);
-    rank_to_value.insert("7", 7);
-    rank_to_value.insert("8", 8);
-    rank_to_value.insert("9", 9);
-    rank_to_value.insert("10", 10);
-    rank_to_value.insert("J", 11);
-    rank_to_value.insert("Q", 12);
-    rank_to_value.insert("K", 13);
-    rank_to_value.insert("A", 14);
-
-    let chunks = hand
-        .split(' ')
+fn rank_hand(hand: &str) -> HandRank {
+    let sorted_hand: Vec<(u16, &str)> = hand
+        .split_whitespace()
         .map(|card| {
-            let rank = &card[..card.len()-1];
-            let value = rank_to_value.get(rank);
-            match value {
-                Some(v) => *v,
-                None => panic!("{} is NOT a valid card!", card),
-            }
+            let (num, suit) = card.split_at(1);
+            let num = num.parse::<u16>().unwrap();
+            (num, suit)
         })
-        .collect::<Vec<u32>>();
+        .collect();
 
-    let result = chunks.iter().max().unwrap();
+    let numbers: LinkedList<_> = sorted_hand.iter().map(|&(num, _)| num).collect();
+    let suits: LinkedList<_> = sorted_hand.iter().map(|&(_, suit)| suit).collect();
+    let mut hand_rank: u16;
+    let mut tie_breaker: &str;
 
-    *result
+    
+
+    HandRank
 }
 
 /*
@@ -60,4 +46,17 @@ C - Clubs
 H - Hearts
 D - Diamonds
 NO RANK in Suits!
+*/
+
+/*
+Straight flush  -> 9
+Four of a kind  -> 8
+Full house      -> 7
+Flush           -> 6
+Straight        -> 5
+Three of a kind -> 4
+Two pair        -> 3
+One pair        -> 2
+High Card       -> 1
+TOTAL : 9 ranks
 */
