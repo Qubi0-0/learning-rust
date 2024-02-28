489bb3cd07bb70ff30551987351bc48998ecb538
Jakub Zeifert <kuba1102@gmail.com>
2024-02-28 11:56:07 +0100

    Refactor sum_of_multiples function to improve performance and readability

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut values = vec![];
+    for factor in factors {
+        if *factor == 0 {
+            continue;
+        }
+        let mut multiplier = *factor;
+        while multiplier < limit {
+            values.push(multiplier);
+            multiplier += factor;
+        }
+    }
+    values.sort();
+    values.dedup();
+    values.iter().sum::<u32>()
 }
