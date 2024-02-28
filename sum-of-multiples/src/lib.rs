8e354b09050a6c716caff825c400e03646f5f7f2
Jakub Zeifert <kuba1102@gmail.com>
2024-02-28 13:12:48 +0100

    Refactor sum_of_multiples function

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,16 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut values = vec![];
-    for factor in factors {
-        if *factor == 0 {
-            continue;
-        }
-        let mut multiplier = *factor;
-        while multiplier < limit {
-            values.push(multiplier);
-            multiplier += factor;
-        }
-    }
-    values.sort();
-    values.dedup();
-    values.iter().sum::<u32>()
+    (1..limit)
+        .filter(|&n| factors.iter().any(|&factor| factor != 0 && n % factor == 0))
+        .sum()
 }
