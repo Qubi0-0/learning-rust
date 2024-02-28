8e73a4923977af77e1caf45e25ac7a36e0c3c5b9
Jakub Zeifert <kuba1102@gmail.com>
2024-02-28 17:27:21 +0100

    Refactor bracket balancing function

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,3 +1,27 @@
-pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+use std::collections::HashMap;
+
+pub fn brackets_are_balanced(s: &str) -> bool {
+    let mut stack = Vec::new();
+    let bracket_pairs: HashMap<char, char> = [('}', '{'), (']', '['), (')', '(')]
+        .iter()
+        .cloned()
+        .collect();
+
+    for ch in s.chars() {
+        match ch {
+            '{' | '[' | '(' => stack.push(ch),
+            '}' | ']' | ')' => {
+                if stack.is_empty() {
+                    return false;
+                }
+                if *stack.last().unwrap() != *bracket_pairs.get(&ch).unwrap() {
+                    return false;
+                }
+                stack.pop();
+            }
+            _ => (),
+        }
+    }
+
+    stack.is_empty()
 }
